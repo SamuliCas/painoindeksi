80571f94c049f657097198c4d97936d1b01eac3c
Samulikoulu <t1casa00@students.oamk.fi>
2022-09-30 11:11:13 +0300

    Jee Valmis!!!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [height, setHeight] = useState('')
+    const [weight, setWeight] = useState('')
+    const [bmi, setBmi] = useState(0)
+  
+
+  function count(e){
+    e.preventDefault()
+    const result = weight / (height * height)
+    setBmi(result)
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Calculatin body mass index</h3>
+      <form onSubmit={count} >
+        <div>
+          <label>Height</label>
+          <input value={height} onChange ={e => setHeight(e.target.value)}/>
+        </div>
+        <div>
+          <label>Weight</label>
+          <input value={weight} onChange ={e => setWeight(e.target.value)}/>      
+        </div>
+        <div>
+        <output>{bmi.toFixed(2)}</output>
+        </div>
+        <button>Calculate</button>
+      </form>
     </div>
   );
-}
+  }
 
 export default App;
